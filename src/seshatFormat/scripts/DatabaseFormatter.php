f55747aad7a52b438e643edf49dad48c94620b6b
NathanDeveloping <winger.nathan.contact@gmail.com>
2017-05-12 15:35:24 +0200

    methode d'ajout de ligne/colonne au fichier excel et génération des champs

--- src/seshatFormat/scripts/DatabaseFormatter.php
+++ src/seshatFormat/scripts/DatabaseFormatter.php
@@ -17,7 +17,28 @@ use PDO;
 class DatabaseFormatter
 {
 
+    /**
+     * $db connexion base de données
+     * @var PDO
+     * $logger log (console et fichier text)
+     */
     private $db, $logger;
+
+    /**
+     * nombres de champs associés pour créer le squelette
+     * du fichier Excel
+     * valeurs associées lors du passage dans le rowToArray()
+     */
+    private $nbOperators, $nbInstitutions, $nbSamplingPoints, $nbMeasurement, $nbSampleSuffix, $nbSampleKind;
+
+    /**
+     * colonne et ligne actuelle
+     * mis à jour lors du addNextLine et addNextColumn
+     */
+    private $currentColumn, $currentLine;
+
+    private $currentExcelObject;
+
 
     /**
      * DatabaseFormatter constructor.
@@ -80,8 +101,121 @@ class DatabaseFormatter
      */
     public function rowToArray($row) {
         if(isset($row)) {
-
+            $this->init();
         }
+    }
+
+    /**
+     * crée le squelette du fichier excel
+     * (les différents intitulé des champs)
+     *
+     * @param $phpExcelObject
+     *          object phpExcel précédemment crée
+     */
+    public function createSkeleton() {
+        $this->currentExcelObject->setActiveSheet(0);
+        $this->currentExcelObject->getActiveSheet()->setTitle('INTRO');
+        $this->addNextLine("TITLE");
+        $this->insertEmptyLine();
+        $this->addNextLine("DATA DESCRIPTION");
+        $this->addNextLine("KEYWORD");
+        $this->addNextLine("FILE CREATOR");
+        for($i = 0; $i < $this->nbOperators; $i++) {
+            $this->addNextLine("NAME");
+            $this->addNextLine("FIRST NAME");
+            $this->addNextLine("MAIL");
+        }
+        $this->addNextLine("CREATION DATE");
+        $this->addNextLine("LANGUAGE");
+        $this->addNextLine("PROJECT NAME");
+        for($i = 0; $i < $this->nbInstitutions; $i++) {
+            $this->addNextLine("INSTITUTION");
+        }
+        $this->addNextLine("SCIENTIFIC FIELD");
+        $this->insertEmptyLine();
+        $this->insertEmptyLine();
+        $this->insertEmptyLine();
+        $this->addNextColumn("SAMPLING POINTS");
+        $this->addNextColumn("COORDONATE SYSTEM");
+        $this->addNextColumn("ABBREVIATION");
+        $this->addNextColumn("LONGITUDE");
+        $this->addNextColumn("LATITUDE");
+        $this->addNextColumn("ELEVATION (m)");
+        $this->addNextColumn("DESCRIPTION");
+        $this->resetColumn();
+        for($i = 0; $i < $this->nbSamplingPoints; $i++) {
+            $this->addNextLine("SAMPLING_POINT");
+        }
+        $this->insertEmptyLine();
+        $this->insertEmptyLine();
+        $this->addNextLine("SAMPLING DATE");
+        $this->insertEmptyLine();
+        $this->insertEmptyLine();
+        $this->insertEmptyLine();
+        $this->insertEmptyLine();
+        for($i = 0; $i < $this->nbSampleKind; $i++) {
+            $this->addNextLine("SAMPLE KIND");
+        }
+        $this->insertEmptyLine();
+        $this->addNextColumn("");
+        $this->addNextColumn("ABBREVIATION");
+        $this->resetColumn();
+        for($i = 0; $i < $this->nbSampleSuffix; $i++) {
+            $this->addNextLine("SAMPLE SUFFIX");
+        }
+        $this->insertEmptyLine();
+        $this->insertEmptyLine();
+        $this->addNextColumn("NATURE OF MEASUREMENT");
+        $this->addNextColumn("ABBREVIATION");
+        $this->addNextColumn("UNIT");
+        $this->resetColumn();
+        for($i = 0; $i < $this->nbMeasurement; $i++) {
+            $this->addNextLine("MEASUREMENT");
+        }
+        $this->insertEmptyLine();
+        $this->addNextLine("METHODOLOGY"); $this->addNextColumn("sampling method"); $this->resetColumn();
+        $this->addNextLine("METHODOLOGY"); $this->addNextColumn("sample conditionning"); $this->resetColumn();
+        $this->addNextLine("METHODOLOGY"); $this->addNextColumn("analysis or measurement method(s)"); $this->resetColumn();
+        $this->addNextLine("METHODOLOGY"); $this->addNextColumn("field campaign report"); $this->resetColumn();
+        $this->addNextLine("METHODOLOGY"); $this->addNextColumn("sample storage"); $this->resetColumn();
+        $this->addNextLine("METHODOLOGY"); $this->addNextColumn("comments"); $this->resetColumn();
+    }
+
+    /**
+     * @param $label
+     */
+    public function addNextLine($label) {
+        $this->currentExcelObject->getActiveSheet()->SetCellValue($this->currentColumn . $this->currentLine, $label);
+        $this->currentLine++;
+    }
+
+    public function addNextColumn($label) {
+        $this->currentExcelObject->getActiveSheet()->SetCellValue($this->currentColumn . $this->currentLine, $label);
+        $this->currentColumn++;
+    }
+
+    public function resetColumn() {
+        $this->currentColumn = 1;
+    }
+
+    public function insertEmptyLine() {
+        $this->resetColumn();
+        $this->addNextLine("");
+    }
+
+    /**
+     * initialise les variables nb champs
+     * lors du passage au traitement d'un
+     * nouveau formulaire
+     */
+    public function init() {
+        $this->nbInstitutions = 0;
+        $this->nbMeasurements = 0;
+        $this->nbOperators = 0;
+        $this->nbSamplingPoints = 0;
+        $this->currentColumn = 'A';
+        $this->currentLine = 1;
+        $this->currentExcelObject = new PHPExcel();
     }
 
 
